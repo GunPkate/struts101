360d3040b08987b69d96a514b79b8a3e000efc03
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-06-06 11:41:42 +0700

    new file base action

---
+++ src/main/java/constants/Constant.java
@@ -0,0 +1,18 @@
+package constants;
+
+public class Constant {
+	private String role;
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	public enum Role{
+		Staff,
+		Manager
+	}
+}
